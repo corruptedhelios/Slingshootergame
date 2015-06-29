182882f3beae8c04b0755b2a5757a42227b9a113
corruptedhelios <mathis.anzeneder@web.de>
2015-06-29 16:18:31 +0200

    gedöns

---
+++ SlingshooterBasic-master/Assets/Scripts/CloudCrafter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudCrafter : MonoBehaviour {
+
+	public int numClouds = 40;
+
+	public Vector3 cloudPosMin;
+	public Vector3 cloudPosMax;
+
+	public GameObject[] cloudPrefabs;
+
+	private GameObject[] cloudInstances;
+
+	void Awake() {
+
+		cloudInstances = new GameObject[numClouds];
+
+		GameObject cloudParent = GameObject.Find("CloudMaker");
+
+		for(int i=0; i<numClouds; i++) {
+	
+			int chosenPrefab = Random.Range(0, cloudPrefabs.Length);
+
+			GameObject cloud = Instantiate(cloudPrefabs [chosenPrefab]);
+
+			Vector3 cPos = Vector3.zero;
+
+			cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+			cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+
+			cloud.transform.position = cPos;
+
+			cloud.transform.parent = cloudParent.transform;
+		
+			cloudInstances[i] = cloud;
+		}
+	}
+}
---
+++ SlingshooterBasic-master/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+	public static FollowCam S;
+
+	public GameObject poi;
+
+	private float camZ;
+
+	void Awake() {
+		S = this;
+		camZ = this.transform.position.z;
+	}
+
+	void Update() {
+		if(poi == null) {
+			return;
+		}
+
+		Vector3 destination = poi.transform.position;
+
+		// if higher than 0 max is dest.y if lower than 0 max is 0
+		destination.x = Mathf.Max (0, destination.x);
+		destination.y = Mathf.Max (0, destination.y);
+
+		destination.z = camZ;
+		transform.position = destination;
+	
+		this.GetComponent<Camera>().orthographicSize = 10 + destination.y;
+	
+	}
+
+
+
+
+}

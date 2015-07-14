a663fd5a03e2b1496ead7fbff3329dd0ef4b41f3
corruptedhelios <mathis.anzeneder@web.de>
2015-07-15 01:48:02 +0200

    Updated models for ball and slingshot. New prefab for ground and start of skripting.

---
+++ SlingshooterBasic-master/Assets/Scripts/GroundCrafter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCrafter : MonoBehaviour {
+
+	public int blockcount = 50;
+
+	public GameObject groundPrefab;
+	
+	private GameObject[] groundInstances;
+	
+	void Awake() {
+		
+		groundInstances = new GameObject[blockcount];
+		
+		GameObject groundParent = GameObject.Find("GroundMaker");
+		
+		for(int i=0; i<blockcount; i++) {
+			
+			float divergence = Random.Range(9.73, 10.09);
+			
+			GameObject ground = Instantiate(groundPrefab);
+
+			Vector3 cPos = Vector3.zero;
+
+			cPos.x = -100 + (i*2.344);
+			cPos.y = -1*divergence;
+			
+			ground.transform.position = cPos;
+			
+			ground.transform.parent = groundParent.transform;
+			
+			groundInstances[i] = ground;
+		}
+}
+
+}
